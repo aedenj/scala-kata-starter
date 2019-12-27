a2330d5ba2d05018e947e58d96eb1b6603107869
Aeden Jameson <aeden.jameson@gmail.com>
2019-12-27 00:48:44 -0800

    fix accidental overwrite

--- src/main/scala/Solution.scala
+++ src/main/scala/Solution.scala
@@ -1,15 +1,5 @@
 package solution
 
 object Solution {
-  def merge(l1:List[Int], l2:List[Int]) : List[Int]= {
-    (l1, l2) match {
-      case (Nil, Nil) => Nil
-      case (x::xs, Nil) => l1
-      case (Nil, y::ys) => l2
-      case (x::xs, y::ys) => {
-        if (x <= y) x :: merge(l1.tail, l2)
-        else y :: merge(l1, l2.tail)
-      }
-    }
-  }
+  def kata() = ???
 }
